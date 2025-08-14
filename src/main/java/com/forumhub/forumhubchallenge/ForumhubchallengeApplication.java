2d3416837e6f0b7ade4cdc7a724910b20be5a636
Vin Martins <marcosviniciossl3@gmail.com>
2025-08-14 13:16:24 -0300

    first one

---
+++ src/main/java/com/forumhub/forumhubchallenge/ForumhubchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.forumhub.forumhubchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forumhub/forumhubchallenge/ForumhubchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forumhub.forumhubchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
